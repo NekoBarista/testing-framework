0bb8352464eca5f6732c55c63f8d7cab4d2ebc31
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-08-08 21:08:10 +1000

    complete

--- runner.js
+++ runner.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const render = require('./render')
 const forbiddenDirs = ['node_modules']
 class Runner {
   constructor() {
@@ -9,14 +10,17 @@ class Runner {
   async runTests() {
     for (let file of this.testFiles) {
       const beforeEaches = [];
+
+      global.render = render;
+
       global.beforeEach = fn => {
         beforeEaches.push(fn);
       };
-      global.it = (desc, fn) => {
+      global.it = async (desc, fn) => {
         beforeEaches.forEach(func => func());
 
         try {
-        fn(); 
+        await fn(); 
 
         console.log(`\tOkay - ${desc}`)
       }
